289fe498b5d29b6ed5db938874a10b940283d50a
Kuheli Manna <constantkuheli5@gmail.com>
2024-07-09 04:06:17 +0000

    varable.config done

---
+++ 01_basic/01_variables.js
@@ -0,0 +1,13 @@
+const accountId = 1234//can not changeable
+let accoundEmail = "kuheli@gmail.com"
+var accountPassword = "2222"
+accountCity = "pune"
+accountPassword = "3333"
+
+/*
+prefer not to use var
+because of block scope and functional scope
+*/
+
+console.log(accountId)
+console.table([accountId, accoundEmail, accountPassword, accountCity] )
