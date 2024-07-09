91259ef3be53654c14ed7938cb2cd83dbed2c970
Kuheli Manna <constantkuheli5@gmail.com>
2024-07-09 04:33:18 +0000

    datatype

---
+++ 01_basic/02_datatype.js
@@ -0,0 +1,28 @@
+"use strict"; //treat all js code as newer version
+
+// alert( 3 + 3)//we are using node js, not brower
+console.log(3 
+    +
+     3)//code readability should be high ,avoide this approach
+console.log("kuheli")    
+
+
+let name = "kuheli"
+let age = 19
+let isLoggedIn = false
+let state;
+
+
+//number => 2 power 53
+//bigint //use in stock market for storing larger number
+//string => ""
+//boolean => true/false
+//null => standalone value
+//undefined =>
+//symbol => for finding unique
+
+
+//object
+
+console.log(typeof null)//null type is object
+console.log(typeof undefined)//undefined
