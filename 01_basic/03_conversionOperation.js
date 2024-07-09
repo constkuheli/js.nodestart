bab00f0550384e8d0371fe66fe92c8051d917d7f
Kuheli Manna <constantkuheli5@gmail.com>
2024-07-09 16:51:22 +0000

    done operations

--- 01_basic/03_conversionOperation.js
+++ 01_basic/03_conversionOperation.js
@@ -24,3 +24,21 @@ let someNumber = 33
 let stringNumber = String(someNumber)
 console.log(stringNumber)
 console.log(typeof stringNumber);
+
+//***************************************Operation****************************** 
+
+let value = 3
+let negValue = -value
+console.log(negValue)
+
+let str1 = "hiiiiii"
+let str2 = "  hello"
+
+let str3 = str1+str2
+console.log(str3)
+
+console.log(2 + "1")//21
+console.log("2" + 1)//21
+console.log("1" + 2 +3)//123//if first element is string thn all are treated as string
+console.log(1 + 2 +"3")//33//if first is number thn the number addition work and last string operation done
+
