913ea8cadefe96660fb0c5fcd023e6a47ed571b4
Kuheli Manna <constantkuheli5@gmail.com>
2024-07-10 05:44:50 +0000

    stack heap concept

--- datatypeSummary.js
+++ datatypeSummary.js
@@ -13,17 +13,46 @@ const heros = ["sk", "pk", "yk"];
  
 //object- store in key value pairs
  
- let myobj = {
+let myobj = {
     name:"kuheli",
     age: 19,
- }
+}
 
  //function
  
- const myFunction = function(){
+const myFunction = function(){
     console.log("Hello World")
- }
+}
 
-  console.log(typeof heros)
+console.log(typeof heros)
 
 //all the non premitive datatype shows as object
+
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+//Stack(premitive)  Heap(non premitive)
+
+
+//in the stack
+let myYoutubename = "kuhelidotcom";
+let anothername = myYoutubename;
+anothername = "sunnydotcom"
+
+console.log(anothername);
+console.log(myYoutubename);
+
+
+//in the heap
+let userone = {
+   email: "user@gmail.com",
+   upi: "user@sbi",
+}
+
+let usertwo = userone;
+
+usertwo.email = "kuheli@gmail.com"
+
+console.log(usertwo.email);
+console.log(userone.email);
+
+//here both the value change because its comes from  heap reference to the stack
