62d3228b6182456e97e2bd8fe47cf0d696a73102
Kuheli Manna <constantkuheli5@gmail.com>
2024-07-10 05:10:14 +0000

    all type of data

---
+++ datatypeSummary.js
@@ -0,0 +1,29 @@
+//premitive
+
+//7 type: String, Number, Boolean, Null, Undefined, Symbol, BigInt
+
+ 
+
+//reference(Non primitive)
+
+//Array, Objects, Functions
+
+//array
+const heros = ["sk", "pk", "yk"];
+ 
+//object- store in key value pairs
+ 
+ let myobj = {
+    name:"kuheli",
+    age: 19,
+ }
+
+ //function
+ 
+ const myFunction = function(){
+    console.log("Hello World")
+ }
+
+  console.log(typeof heros)
+
+//all the non premitive datatype shows as object
