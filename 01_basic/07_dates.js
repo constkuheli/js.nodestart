43232bc9bca8d80881e47f0cbb4078005bc4f1a5
Kuheli Manna <constantkuheli5@gmail.com>
2024-07-11 04:48:34 +0000

    dates

--- 01_basic/07_dates.js
+++ 01_basic/07_dates.js
@@ -17,3 +17,10 @@ console.log(myCreatedDate.toDateString());
 let myTimeStamp = Date.now()
 console.log(myTimeStamp);//1st january 1970 to till now  the milisecond value
 
+let myCreatedDate1 = new Date("01-14-2023")
+console.log(myCreatedDate1.getTime());
+
+console.log(Math.floor(Date.now()/1000));//for converting the date in second
+
+let 
+
